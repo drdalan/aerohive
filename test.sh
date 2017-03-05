349d185603bce4ea6a3a27761ad84be215b62108
Dr Dalan <drdalan@users.noreply.github.com>
2017-03-04 18:36:18 -0800

    Delete test.sh

--- test.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-#/opt/aerohive/backend/model/admin.js
-
-HOSTNAME="$( hostname )"
-IPADDRESS="$( ip addr | grep -Po 'inet \K[\d.]+' | grep -v 127.0.0.1 )"
-
-echo $HOSTNAME
-echo $IPADDRESS
