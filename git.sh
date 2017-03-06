6115b7a70c290e52b4008d824cdb49e3f7126437
Dr Dalan <drdalan@users.noreply.github.com>
2017-03-05 21:14:06 -0800

    Update git.sh

--- git.sh
+++ git.sh
@@ -7,28 +7,28 @@ IPADDRESS="$( ip addr | grep -Po 'inet \K[\d.]+' | grep -v 127.0.0.1 )"
 USERNAME="$( cat /home/app/info | grep USERNAME | cut -c 10-  )"
 PASSWORD="$( cat /home/app/info | grep PASSWORD | cut -c 10-  )"
 
-#local_branch=$(git rev-parse --symbolic-full-name --abbrev-ref HEAD)
-#remote_branch=$(git rev-parse --abbrev-ref --symbolic-full-name @{u})
-#remote=$(git config branch.$local_branch.remote)
-#echo "Fetching from $remote..."
+local_branch=$(git rev-parse --symbolic-full-name --abbrev-ref HEAD)
+remote_branch=$(git rev-parse --abbrev-ref --symbolic-full-name @{u})
+remote=$(git config branch.$local_branch.remote)
+echo "Fetching from $remote..."
 
-#git fetch $remote
+git fetch $remote
 
-#if git merge-base --is-ancestor $remote_branch HEAD; then
-#    echo 'Already up-to-date'
-#    exit 0
+if git merge-base --is-ancestor $remote_branch HEAD; then
+    echo 'Already up-to-date'
+    exit 0
 
-#fi
+fi
 
-#if git merge-base --is-ancestor HEAD $remote_branch; then
-#    echo 'Fast-forward possible. Merging...'
-#    git merge --ff-only --stat $remote_branch
+if git merge-base --is-ancestor HEAD $remote_branch; then
+    echo 'Fast-forward possible. Merging...'
+    git merge --ff-only --stat $remote_branch
 
-#else
-#    echo 'Fast-forward not possible. Rebasing...'
-#    git rebase --preserve-merges --stat $remote_branch
+else
+    echo 'Fast-forward not possible. Rebasing...'
+    git rebase --preserve-merges --stat $remote_branch
 
-#fi
+fi
 
 cd /opt/aerohive/
 git reset --hard origin/master
